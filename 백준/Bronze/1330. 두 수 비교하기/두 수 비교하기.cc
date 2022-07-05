fe01ab4208c7d34738abf8692c4b2ecd5bb8efc1
min9yu98 <76439068+min9yu98@users.noreply.github.com>
2022-07-05 22:27:06 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]){
+    int a, b;
+    
+    cin >> a >> b;
+    
+    if (a > b) cout << ">";
+    else if (a == b) cout << "==";
+    else cout << "<";
+    
+}
+
