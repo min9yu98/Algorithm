8784765106607a7ee0332bb157494f1ff877b6ee
min9yu98 <76439068+min9yu98@users.noreply.github.com>
2022-07-05 22:05:19 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]){
+    int a, b;
+    cin >> a >> b;
+    
+    cout << a + b << endl;
+    cout << a - b << endl;
+    cout << a * b << endl;
+    cout << a / b << endl;
+    cout << a % b << endl;
+}
+
