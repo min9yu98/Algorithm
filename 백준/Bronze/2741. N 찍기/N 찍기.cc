2c2774065931fedeb9bcb4c2c2f22cf9bf10ff26
min9yu98 <76439068+min9yu98@users.noreply.github.com>
2022-07-06 17:57:52 +0900

    [Bronze V] Title: N 찍기, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include<algorithm>
+
+using namespace std;
+
+int main(int argc, char const *argv[]){
+    ios_base::sync_with_stdio(false);    // C, C++ 동기화 해제
+    cin.tie(NULL);    // 입력과 출력을 분리
+     
+    int T;
+    cin >> T;
+    for(int i = 1; i <= T; i++) {
+        cout << i << "\n"; // endl 대신 \n 을 쓰기
+    }
+}
+
