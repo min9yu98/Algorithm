feb7058752423bd8c1a276b32a77910151070703
min9yu98 <76439068+min9yu98@users.noreply.github.com>
2022-07-06 19:14:33 +0900

    [Bronze V] Title: A+B - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<algorithm>
+
+using namespace std;
+
+int main(int argc, char const *argv[]){
+    ios_base::sync_with_stdio(false);    // C, C++ 동기화 해제
+    cin.tie(NULL);    // 입력과 출력을 분리
+
+    int a, b;
+    while (cin >> a >> b) {
+            cout << a + b << "\n";
+    }
+}
+
