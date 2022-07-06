6d3a1197eca023d3fe9aaae5604e96c21395b64d
min9yu98 <76439068+min9yu98@users.noreply.github.com>
2022-07-06 19:02:39 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<algorithm>
+
+using namespace std;
+
+int main(int argc, char const *argv[]){
+    ios_base::sync_with_stdio(false);    // C, C++ 동기화 해제
+    cin.tie(NULL);    // 입력과 출력을 분리
+     
+    int T;
+    cin >> T;
+    for(int i = 0; i < T; i++) {
+        for (int j = 0; j <= i; j++) cout << "*";
+        cout << endl;
+    }
+}
+
