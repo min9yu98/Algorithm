fccd0630f138ac83ae2ebaebc4d37e338684a5b1
min9yu98 <76439068+min9yu98@users.noreply.github.com>
2022-07-12 17:58:28 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <math.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[]){
+    ios_base::sync_with_stdio(false);    // C, C++ 동기화 해제
+    cin.tie(NULL);    // 입력과 출력을 분리
+    
+    char c;
+    cin >> c;
+    
+    printf("%d\n", c);
+}
