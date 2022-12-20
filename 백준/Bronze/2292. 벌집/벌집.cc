8da6406ddcf3b218d28a8b464e7c8532c322fa97
min9yu98 <76439068+min9yu98@users.noreply.github.com>
2022-12-20 11:07:12 +0900

    [Bronze II] Title: 벌집, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.cc
+++ 백준/Bronze/2292. 벌집/벌집.cc
@@ -20,19 +20,13 @@ int n;
 int main(){
     cin >> n;
     
-    if (n == 1) {
-        cout << 1 << endl;
-        return 0;
-    }
-    int i = 0;
-    while (true){
-        if (2 + 3 * (i * i) - 3 * i <= n && n <= 1 + 3 * (i * i) + 3 * i){
-            i++;
-            cout << i << endl;
-            break;
-        }
-        i++;
-    }
+    int i = 1;
+    int cnt = 1;
     
+    while (n > i){
+        i += (6 * cnt);
+        cnt++;
+    }
+    cout << cnt << endl;
     return 0;
 }
