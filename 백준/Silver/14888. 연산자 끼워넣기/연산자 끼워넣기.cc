65784aad911b302984eaf9fc6459362885427949
MinGyu <76439068+min9yu98@users.noreply.github.com>
2023-02-24 00:16:20 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.cc
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <set>
+#include <ostream>
+#include <string>
+#include <cstring>
+#include <sstream>
+#include <time.h>
+#include <queue>
+#include <cstdlib>
+#include <stdbool.h>
+#include <map>
+
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+
+int oper[4] = {0, 0, 0, 0};
+int n, maxi = -1000000001, mini = 1000000001;
+int v[11];
+
+int calc(int a, int op, int b){
+    if (op == 0)
+        return a + b;
+    else if (op == 1)
+        return a - b;
+    else if (op == 2)
+        return a * b;
+    else
+        return a / b;
+}
+
+void dp(int result, int idx){
+    if (idx == n){
+        if (result > maxi)
+            maxi = result;
+        if (result < mini)
+            mini = result;
+        return;
+    }
+    
+    for (int i = 0; i < 4; i++){
+        if (oper[i] != 0) {
+            oper[i]--;
+            if (i == 0)
+                dp(result + v[idx], idx + 1);
+            else if (i == 1)
+                dp(result - v[idx], idx + 1);
+            else if (i == 2)
+                dp(result * v[idx], idx + 1);
+            else if (i == 3)
+                dp(result / v[idx], idx + 1);
+            oper[i]++;
+        }
+    }
+}
+
+int main(){
+    fastio;
+    
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        cin >> v[i];
+    
+    for (int i = 0; i < 4; i++)
+        cin >> oper[i];
+    
+    dp(v[0], 1);
+    
+    cout << maxi << '\n' << mini << '\n';
+    
+    return 0;
+}
