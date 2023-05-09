d5f053a491e009c8175fd75a3cb61c48d9b62616
MinGyu <kimmingyu9811@gmail.com>
2023-05-09 14:33:09 +0900

    [Silver I] Title: 회의실 배정, Time: 4084 ms, Memory: 52820 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -0,0 +1,11 @@
+n = int(input())
+time = sorted([tuple(map(int, input().split())) for _ in range(n)], key=lambda x:(x[1], x[0]))
+ans = end = 0
+for s, e in time:
+    if s >= end:
+        ans += 1
+        end = e
+print(ans)
+
+
+# 블로그 참고
