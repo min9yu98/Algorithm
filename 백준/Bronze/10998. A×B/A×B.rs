84f710bc7105e2840e5566101282436ad0202207
MinGyu <kimmingyu9811@gmail.com>
2023-11-01 16:31:05 +0900

    [Bronze V] Title: A×B, Time: 4 ms, Memory: 13208 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.rs
@@ -0,0 +1,10 @@
+use std::io;
+
+fn main() {
+    let mut num_array = String::new();
+    io::stdin().read_line(&mut num_array).unwrap();
+    let numbers: Vec<&str> = num_array.split_whitespace().collect();
+    let number_first = numbers[0].parse::<i32>().unwrap();
+    let number_second = numbers[1].parse::<i32>().unwrap();
+    println!("{}", number_first * number_second);
+}
