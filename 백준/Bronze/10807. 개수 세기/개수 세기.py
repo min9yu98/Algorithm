c474573f19e9b54b49d2f7535e53481964089f30
MinGyu <kimmingyu9811@gmail.com>
2024-01-03 21:37:33 +0900

    [Bronze V] Title: 개수 세기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.py
+++ 백준/Bronze/10807. 개수 세기/개수 세기.py
@@ -2,7 +2,7 @@ import sys
 _input = sys.stdin.readline
 
 n = int(_input())
-s = list(map(int, _input().split()))
-v = int(_input())
+s = _input().strip().split()
+v = _input().strip()
 
 print(s.count(v))
