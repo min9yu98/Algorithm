b00e872964a07aafa5df6ef0a2eea3be459f0b7f
MinGyu <kimmingyu9811@gmail.com>
2024-01-05 00:36:29 +0900

    [Silver II] Title: DFS와 BFS, Time: 80 ms, Memory: 34072 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,38 +1,46 @@
 import sys
 from collections import deque
+_input = sys.stdin.readline
 
-
-n, m, v = map(int, input().split())
+n, m, v = map(int, _input().split())
+rep = [[] for _ in range(n + 1)]
 visited_dfs = [False for _ in range(n + 1)]
 visited_bfs = [False for _ in range(n + 1)]
-graph = [[False] * (n + 1) for _ in range(n + 1)]
-
 for _ in range(m):
-    a, b = map(int, input().split())
-    graph[a][b] = True
-    graph[b][a] = True
+    a, b = map(int, _input().split())
+    rep[a].append(b)
+    rep[a].sort()
+    rep[b].append(a)
+    rep[b].sort()
+result_dfs = []
+result_bfs = []
 
 
-def bfs(element):
-    q = deque([element])
-    visited_bfs[element] = True
-    while q:
-        element = q.popleft()
-        print(element, end=" ")
-        for i in range(1, n + 1):
-            if not visited_bfs[i] and graph[element][i]:
-                q.append(i)
-                visited_bfs[i] = True
+def dfs(next_item):
+    if visited_dfs[next_item]:
+        return
+    result_dfs.append(str(next_item))
+    visited_dfs[next_item] = True
+    for e in rep[next_item]:
+        if not visited_dfs[e]:
+            dfs(e)
+    return
 
-
-def dfs(element):
-    visited_dfs[element] = True
-    print(element, end=" ")
-    for i in range(1, n + 1):
-        if not visited_dfs[i] and graph[element][i]:
-            dfs(i)
+def bfs(start):
+    visited_bfs[start] = True
+    queue = deque([start])
+    while queue:
+        vt = queue.popleft()
+        result_bfs.append(str(vt))
+        for e in rep[vt]:
+            if not visited_bfs[e]:
+                queue.append(e)
+                visited_bfs[e] = True
+    return
 
 
 dfs(v)
-print()
 bfs(v)
+
+print(' '.join(result_dfs))
+print(' '.join(result_bfs))
