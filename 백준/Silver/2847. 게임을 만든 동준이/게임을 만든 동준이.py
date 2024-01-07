2ebdfe71fc15e95e86442a1f62088f90f3312e94
MinGyu <kimmingyu9811@gmail.com>
2024-01-08 02:53:51 +0900

    [Silver IV] Title: 게임을 만든 동준이, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2847. 게임을 만든 동준이/게임을 만든 동준이.py
+++ 백준/Silver/2847. 게임을 만든 동준이/게임을 만든 동준이.py
@@ -4,16 +4,9 @@ input = sys.stdin.readline
 n = int(input())
 score = [int(input()) for _ in range(n)]
 
-if n == 1:
-    print(0)
-else:
-    result = 0
-    for i in range(1, n):
-        if score[i - 1] >= score[i]:
-            result += score[i - 1] - score[i] + 1
-            score[i - 1] = score[i] - 1
-    for i in range(n - 1, 0, -1):
-        if score[i - 1] >= score[i]:
-            result += score[i - 1] - score[i] + 1
-            score[i - 1] = score[i] - 1
-    print(result)
+result = 0
+for i in range(n - 1, 0, -1):
+    if score[i - 1] >= score[i]:
+        result += score[i - 1] - score[i] + 1
+        score[i - 1] = score[i] - 1
+print(result)
