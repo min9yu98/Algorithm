5a6835ce855d30a80ef4bd75e85bac46d68f28fd
MinGyu <kimmingyu9811@gmail.com>
2024-01-10 00:06:52 +0900

    [Silver V] Title: 국회의원 선거, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1417. 국회의원 선거/국회의원 선거.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+dasom = int(input())
+vote = []
+cnt = 0
+for _ in range(n - 1):
+    a = int(input())
+    vote.append(a)
+vote.sort(reverse=True)
+if n == 1:
+    print(0)
+else:
+    while vote[0] >= dasom:
+        dasom += 1
+        vote[0] -= 1
+        cnt += 1
+        vote.sort(reverse=True)
+    print(cnt)
