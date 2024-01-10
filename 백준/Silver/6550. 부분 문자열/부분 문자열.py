a6f2d3b3d61f9577ae0df980f34bf1a635ef0c95
MinGyu <kimmingyu9811@gmail.com>
2024-01-10 23:14:21 +0900

    [Silver V] Title: 부분 문자열, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/6550. 부분 문자열/부분 문자열.py
+++ 백준/Silver/6550. 부분 문자열/부분 문자열.py
@@ -1,20 +1,21 @@
+import sys
+
+input = sys.stdin.readline
+
 while True:
     try:
-        s, t = input().split()
-
-        value = 0
-        flag = 0
-        for i in range(len(t)):
-            if t[i] == s[value]:
-                value += 1
-                if value == len(s):
-                    flag = 1
+        s, t = input().strip().split()
+        flag = False
+        idx = 0
+        for w in t:
+            if w == s[idx]:
+                idx += 1
+                if len(s) == idx:
+                    flag = True
                     break
-
-        if flag == 1:
+        if flag:
             print('Yes')
         else:
             print('No')
-
     except:
         break
