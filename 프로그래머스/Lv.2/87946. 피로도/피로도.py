447c4d4d02ba1a1d98f22da6d26defb0056e9b63
MinGyu <kimmingyu9811@gmail.com>
2024-01-13 01:04:34 +0900

    [level Lv.2] Title: 피로도, Time: 0.09 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/Lv.2/87946. 피로도/피로도.py
+++ 프로그래머스/Lv.2/87946. 피로도/피로도.py
@@ -1,18 +1,19 @@
-from copy import deepcopy
-from itertools import permutations
+answer = 0
+visited = []
 
-def solution(k, dungeons):
-    answer = []
-    tmp = deepcopy(k)
-    for comb in list(permutations(dungeons, len(dungeons))):
-        cnt = 0
-        for e in comb:
-            if k >= e[0] and k >= e[1]:
-                cnt += 1
-                k -= e[1]
-            else:
-                break
-        answer.append(cnt)
-        cnt = 0
-        k = tmp
-    return max(answer)
+def dfs(k, dg, cnt):
+    global answer
+    if cnt > answer:
+        answer = cnt
+    for i in range(len(dg)):
+        if k >= dg[i][0] and not visited[i]:
+            visited[i] = True
+            dfs(k - dg[i][1], dg, cnt + 1)
+            visited[i] = False
+
+
+def solution(k, dg):
+    global visited
+    visited = [False] * len(dg)
+    dfs(k, dg, 0)
+    return answer
