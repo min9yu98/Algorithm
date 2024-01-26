2b6323a3f3518b430c307e66c4fdd8665fe34363
MinGyu <kimmingyu9811@gmail.com>
2024-01-26 23:54:29 +0900

    [Bronze II] Title: 공 바꾸기, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -0,0 +1,6 @@
+n, m = map(int, input().split())
+basket = [i for i in range(1, n + 1)]
+for _ in range(m):
+    a, b = map(int, input().split())
+    basket[a - 1], basket[b - 1] = basket[b - 1], basket[a - 1]
+print(' '.join(list(map(str, basket))))
