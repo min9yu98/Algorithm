69dca45dafa603fdc6ec28a9d7e6fef0b717f7d9
MinGyu <kimmingyu9811@gmail.com>
2024-01-30 23:49:55 +0900

    [Gold IV] Title: 연구소, Time: 2636 ms, Memory: 34116 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -8,6 +8,7 @@ move_x = [0, 0, -1, 1]
 move_y = [1, -1, 0, 0]
 
 
+# bfs에서 이미 벽을 쌓은 t_graph안에 바이러스를 확산시킨 후 0의 갯수 카운트 및 max값 갱신
 def bfs(t_graph):
     global answer
     queue = deque([(j, i) for j in range(m) for i in range(n) if t_graph[i][j] == 2])
@@ -25,10 +26,11 @@ def bfs(t_graph):
 
 n, m = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(n)]
-tmp_wall_graph = [(x, y) for x in range(m) for y in range(n) if not graph[y][x]]
+tmp_wall_graph = [(x, y) for x in range(m) for y in range(n) if not graph[y][x]]  # 벽을 쌓을 수 있는 공간
 answer = 0
 
-for comb in combinations(tmp_wall_graph, 3):
+# 그래프 별로 벽을 조합에 따라 세운 후 그 그래프를 bfs 적용
+for comb in combinations(tmp_wall_graph, 3):  # 3개의 벽을 쌓기 위해 모든 벽을 쌓을 수 있는 공간에서 3부분만 조합한 후 반복문 실행
     tmp_graph = deepcopy(graph)
     for x, y in comb:
         tmp_graph[y][x] = 1
