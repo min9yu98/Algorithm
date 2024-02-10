43b57beaae745e4f5f582d9eccecc22a3fbd12ae
MinGyu <kimmingyu9811@gmail.com>
2024-02-10 19:44:34 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 2212 ms, Memory: 31900 KB -BaekjoonHub

---
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.py
@@ -0,0 +1,22 @@
+s = input().strip() + ' '
+stack = []
+result = ''
+cnt = 0
+for i in s:
+    if i == '<':
+        cnt = 1
+        for _ in range(len(stack)):
+            result += stack.pop()
+    stack.append(i)
+
+    if i == '>':
+        cnt = 0
+        for _ in range(len(stack)):
+            result += stack.pop(0)
+
+    if i == ' ' and cnt == 0:
+        stack.pop()
+        for _ in range(len(stack)):
+            result += stack.pop()
+        result += ' '
+print(result)
