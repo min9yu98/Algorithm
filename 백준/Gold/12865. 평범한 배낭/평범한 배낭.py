c17afe2ddfbd6d341e06864f5582b1d3483e5068
MinGyu <kimmingyu9811@gmail.com>
2024-02-13 23:37:55 +0900

    [Gold V] Title: 평범한 배낭, Time: 5972 ms, Memory: 279260 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -0,0 +1,16 @@
+n, k = map(int, input().split())
+knapsack = [[0] * (k + 1) for _ in range(n + 1)]
+stuff = [[0, 0]]
+for _ in range(n):
+    stuff.append(list(map(int, input().split())))
+
+for i in range(1, n + 1):
+    for j in range(1, k + 1):
+        weight = stuff[i][0]
+        value = stuff[i][1]
+
+        if j < weight:
+            knapsack[i][j] = knapsack[i - 1][j]
+        else:
+            knapsack[i][j] = max(value + knapsack[i - 1][j - weight], knapsack[i - 1][j])
+print(knapsack[n][k])
