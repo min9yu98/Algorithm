47fa0b989ac7762733391824e330a702e02cf90f
MinGyu <kimmingyu9811@gmail.com>
2024-02-20 22:11:08 +0900

    [Gold V] Title: 전깃줄, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/2565. 전깃줄/전깃줄.py
@@ -0,0 +1,9 @@
+n = int(input())
+lines = [list(map(int, input().split())) for _ in range(n)]
+dp = [1] * n
+lines.sort()
+for i in range(n):
+    for j in range(i):
+        if lines[i][1] > lines[j][1]:
+            dp[i] = max(dp[i], dp[j] + 1)
+print(n - max(dp))
