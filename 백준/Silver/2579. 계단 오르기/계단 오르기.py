c48a1a9da5536e73293c37082e8db1ad559d6515
MinGyu <kimmingyu9811@gmail.com>
2024-02-28 20:29:07 +0900

    [Silver III] Title: 계단 오르기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -0,0 +1,15 @@
+import sys
+sys.setrecursionlimit(10 ** 9)
+input = sys.stdin.readline
+
+n = int(input())
+stairs = [0] * 301
+for i in range(1, n + 1):
+    stairs[i] = int(input())
+dp = [0] * 301
+dp[1], dp[2], dp[3] = stairs[1], stairs[1] + stairs[2], max(stairs[1] + stairs[3], stairs[2] + stairs[3])
+
+for i in range(4, n + 1):
+    dp[i] = max(dp[i - 3] + stairs[i - 1] + stairs[i], dp[i - 2] + stairs[i])
+print(dp[n])
+
