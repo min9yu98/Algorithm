eb452070552f2bdd67f4eacfd2fb8e507db812b3
MinGyu <kimmingyu9811@gmail.com>
2024-03-20 23:16:51 +0900

    [Silver II] Title: 쇠막대기, Time: 60 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.py
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.py
@@ -1,23 +1,23 @@
 import sys
+sys.setrecursionlimit(10 ** 9)
 
 input = sys.stdin.readline
 
 laser = input().strip()
 laser = laser.replace("()", "!")
-stack = []
 cnt = 0
+push_cnt = 0
 pop_cnt = 0
 for i in range(len(laser)):
     if laser[i] == '(':
-        stack.append(laser[i])
+        push_cnt += 1
     elif laser[i] == '!':
-        cnt += len(stack)
+        cnt += push_cnt
         if pop_cnt != 0:
-            for _ in range(pop_cnt):
-                stack.pop()
+            push_cnt -= pop_cnt
             pop_cnt = 0
     else:
         pop_cnt += 1
 if pop_cnt != 0:
-    cnt += len(stack)
+    cnt += push_cnt
 print(cnt)
