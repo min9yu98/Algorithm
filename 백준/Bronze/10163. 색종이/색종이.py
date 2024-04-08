068967a88aa3169d013d6b965fae5e44544626b4
MinGyu <kimmingyu9811@gmail.com>
2024-04-08 12:12:59 +0900

    [Bronze I] Title: 색종이, Time: 196 ms, Memory: 38316 KB, Score: 53 point -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.py
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -1,9 +1,10 @@
 import sys
+from itertools import combinations
 
 input = sys.stdin.readline
 
 n = int(input())
-graph = [[0] * 102 for _ in range(102)]
+graph = [[0] * 1002 for _ in range(1002)]
 for i in range(1, n + 1):
     x1, y1, x2, y2 = map(int, input().split())
     for j in range(y1, y1 + y2):
@@ -11,6 +12,6 @@ for i in range(1, n + 1):
             graph[j][k] = i
 for i in range(1, n + 1):
     cnt = 0
-    for j in range(102):
+    for j in range(1002):
         cnt += graph[j].count(i)
     print(cnt)
