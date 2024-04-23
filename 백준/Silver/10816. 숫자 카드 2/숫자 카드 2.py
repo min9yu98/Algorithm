6f46b94ab9d83e3e15a69627dc2cb76e47369469
MinGyu <kimmingyu9811@gmail.com>
2024-04-23 20:29:51 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1076 ms, Memory: 118748 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -16,19 +16,8 @@ for card in having:
     else:
         dic[card] += 1
 
-
-def binary(arr, target, start, end):
-    if start > end:
-        return 0
-
-    mid = (start + end) // 2
-    if arr[mid] == target:
-        return dic.get(target)
-    elif arr[mid] < target:
-        return binary(arr, target, mid + 1, end)
-    elif arr[mid] > target:
-        return binary(arr, target, start, mid - 1)
-
-
-for target in numbers:
-    print(binary(having, target, 0, n - 1), end=" ")
+for e in numbers:
+    if dic.get(e) == None:
+        print(0, end=" ")
+    else:
+        print(dic.get(e), end=" ")
