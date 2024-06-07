71fee0de2c38594406d47d219bef5cb4d4ddea84
MinGyu <kimmingyu9811@gmail.com>
2024-06-07 10:42:35 +0900

    [Bronze IV] Title: No Brainer, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/4562. No Brainer/No Brainer.py
@@ -0,0 +1,10 @@
+import sys
+
+input = sys.stdin.readline
+
+for _ in range(int(input())):
+    x, y = map(int, input().split())
+    if x < y:
+        print("NO BRAINS")
+    else:
+        print("MMM BRAINS")
