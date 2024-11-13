0522988ebea11658dc63acaed92d9bbd4cafaabd
MinGyu <kimmingyu9811@gmail.com>
2024-11-13 21:02:48 +0900

    [Silver III] Title: 수 이어 쓰기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1515. 수 이어 쓰기/수 이어 쓰기.py
+++ 백준/Silver/1515. 수 이어 쓰기/수 이어 쓰기.py
@@ -1,12 +1,14 @@
-nums = input()
-i = 0
+import sys
+
+input = sys.stdin.readline
+
+n = list(input().strip())
+num, idx = 0, 0
 while True:
-    i += 1
-    num = str(i)
-    while len(num) > 0 and len(nums) > 0:
-        if num[0] == nums[0]:
-            nums = nums[1:]
-        num = num[1:]
-    if nums == '':
-        print(i)
-        break
+    num += 1
+    for s in str(num):
+        if n[idx] == s:
+            idx += 1
+            if idx >= len(n):
+                print(num)
+                exit(0)
