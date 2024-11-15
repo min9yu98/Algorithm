840b246c3214cb3c17ebfbb62e8781ff43f28142
MinGyu <kimmingyu9811@gmail.com>
2024-11-15 19:10:02 +0900

    [Silver II] Title: KCPC, Time: 128 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/3758. KCPC/KCPC.py
+++ 백준/Silver/3758. KCPC/KCPC.py
@@ -1,42 +1,22 @@
 import sys
+
 input = sys.stdin.readline
 
-
-def sum_lst(lst, idx):
-    total = 0
-    for element in lst[idx]:
-        if element:
-            total += max(element)
-    return total
-
-
-def count_q(lst, idx):
-    cnt = 0
-    for element in lst[idx]:
-        if element:
-            cnt += len(element)
-    return cnt
-
-
 for _ in range(int(input())):
-    n, k, t, m = map(int, input().split())  # n: 팀수, k: 문제 수, t: 나의 팀 id, m: 로그 엔트리 수
-    check_q = [[0] * (k + 1) for _ in range(n + 1)]
-    score_lst = [[[] for _ in range(k + 1)] for _ in range(n + 1)]
-    submit_rank = [0] * (n + 1)
-    teams = [[0, 0, 0, 0]]
+    n, k, t, m = map(int, input().split())
+    score_dict = {i: {j: 0 for j in range(1, k + 1)} for i in range(1, n + 1)}
+    submit_cnt = {i: 0 for i in range(1, n + 1)}
+    last_submit = {i: 0 for i in range(1, n + 1)}
     for idx in range(m):
         i, j, s = map(int, input().split())
-        check_q[i][j] = 1
-        score_lst[i][j].append(s)
-        submit_rank[i] = idx + 1
-    for i in range(1, n + 1):
-        total_score = sum_lst(score_lst, i)
-        total_solved_q = count_q(score_lst, i)
-        last_submit = submit_rank[i]
-        teams.append([i, total_score, total_solved_q, last_submit])
-    teams.sort(key=lambda x: (-x[1], x[2], x[3]))
-
-    for i in range(n):
-        if teams[i][0] == t:
+        score_dict[i][j] = max(score_dict[i][j], s)
+        submit_cnt[i] += 1
+        last_submit[i] = idx
+    score_lst = []
+    for idx, scores in score_dict.items():
+        score_lst.append([idx, sum(scores.values()), submit_cnt[idx], last_submit[idx]])
+    score_lst.sort(key=lambda x: (-x[1], x[2], x[3]))
+    for i in range(len(score_lst)):
+        if score_lst[i][0] == t:
             print(i + 1)
             break
