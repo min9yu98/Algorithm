c601579cf47f7f746908832440a2277546d86496
MinGyu <kimmingyu9811@gmail.com>
2024-11-15 19:22:12 +0900

    [Silver III] Title: 타노스, Time: 48 ms, Memory: 31388 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/20310. 타노스/타노스.py
+++ 백준/Silver/20310. 타노스/타노스.py
@@ -1,2 +1,8 @@
-s = list(map(int, input()))
-print('0' * (s.count(0) // 2) + '1' * (s.count(1) // 2))
+lst = list(input())
+zero = lst.count('0') // 2
+one = lst.count('1') // 2
+for _ in range(zero):
+    lst.pop(-(lst[::-1].index('0')) - 1)
+for _ in range(one):
+    lst.pop(lst.index('1'))
+print(''.join(lst))
