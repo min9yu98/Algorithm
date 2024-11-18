6e423b3f89ab18bd1f1e1ecab0c6749adf63acc1
MinGyu <kimmingyu9811@gmail.com>
2024-11-18 21:06:35 +0900

    [Bronze V] Title: 꼬마 정민, Time: 168 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long A = sc.nextLong(), B = sc.nextLong(), C = sc.nextLong();
+		System.out.println(A + B + C);
+
+	}
+}
