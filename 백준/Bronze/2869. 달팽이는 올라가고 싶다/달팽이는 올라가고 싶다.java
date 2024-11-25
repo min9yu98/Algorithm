3ff457294b76b62477648faa4232c2c65cfdcc69
MinGyu <kimmingyu9811@gmail.com>
2024-11-25 18:43:33 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 112 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken()), v = Integer.parseInt(st.nextToken());
+		if (a >= v) {
+			System.out.println(1);
+		} else {
+			if ((v - a) % (a - b) == 0) {
+				System.out.println((v - a) / (a - b) + 1);
+			} else {
+				System.out.println((v - a) / (a - b) + 2);
+			}
+		}
+	}
+}
