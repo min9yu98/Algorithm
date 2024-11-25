b6e5b6c527fabbf7ad7c2834fb771ad751a9ede9
MinGyu <kimmingyu9811@gmail.com>
2024-11-25 19:57:03 +0900

    [Silver I] Title: 그림, Time: 356 ms, Memory: 45816 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.java
+++ 백준/Silver/1926. 그림/그림.java
@@ -8,50 +8,53 @@ public class Main {
 	static int[][] map;
 	static boolean[][] visit;
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer tk = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(tk.nextToken());
-		int m = Integer.parseInt(tk.nextToken());
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken()), m = Integer.parseInt(st.nextToken());
 		Queue<int[]> q = new LinkedList<>();
 
-		map = new int [n][m];
+		map = new int[n][m];
 		visit = new boolean[n][m];
 
-		for(int i=0;i<n;i++){
-			tk = new StringTokenizer(br.readLine());
-			for(int j=0;j<m;j++){
-				map[i][j] = Integer.parseInt(tk.nextToken());
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < m; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		int max = 0;    //그림의 최댓값
-		int num = 0;    //그림의수
 
-		for(int i=0;i<n;i++){
-			for(int j=0;j<m;j++){
-				if(map[i][j] == 0 || visit[i][j]) continue;
+		int max = 0;
+		int num = 0;
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if (map[i][j] == 0 || visit[i][j]) continue;
 				num++;
 				visit[i][j] = true;
-				q.offer(new int[] {i,j});
-				int area = 0 ;
-				while(!q.isEmpty()){
+				q.offer(new int[] {i, j});
+				int area = 0;
+				while (!q.isEmpty()) {
 					area++;
 					int[] temp = q.poll();
-					int x = temp[0];
-					int y = temp[1];
+					int x = temp[0], y = temp[1];
 
-					for(int dir = 0;dir < 4; dir++){
-						int nx = x + dx[dir];
-						int ny = y + dy[dir];
-						if(nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
-						if(visit[nx][ny] || map[nx][ny] !=1) continue;
-						visit[nx][ny] = true;
-						q.offer(new int[]{nx,ny});
+					for (int dir = 0; dir < 4; dir++) {
+						int nx = x + dx[dir], ny = y + dy[dir];
+						if (nx >= 0 && nx < n && ny >= 0 && ny < m) {
+							if (!visit[nx][ny] && map[nx][ny] == 1) {
+								visit[nx][ny] = true;
+								q.offer(new int[]{nx, ny});
+							}
+						}
 					}
 				}
-				max = Math.max(max,area);
+				max = Math.max(max, area);
 			}
 		}
+
 		System.out.println(num);
 		System.out.println(max);
 	}
