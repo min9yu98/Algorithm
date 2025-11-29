17ef9865af684e23a157586a7748e82f7cdbfd6f
MinGyu <kimmingyu9811@gmail.com>
2025-11-29 23:32:05 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 96 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
@@ -0,0 +1,21 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int n = Integer.parseInt(st.nextToken());
+
+		int result = 0;
+		while (n >= 5) {
+			result += n / 5;
+			n /= 5;
+		}
+		System.out.println(result);
+	}
+}
