9d91c2239231cb9db1e68a38db9b8c35e99cc716
MinGyu <kimmingyu9811@gmail.com>
2025-12-12 00:17:18 +0900

    [Silver IV] Title: solved.ac, Time: 324 ms, Memory: 37852 KB -BaekjoonHub

---
+++ 백준/Silver/18110. solved．ac/solved．ac.java
@@ -0,0 +1,32 @@
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int stand = (int) Math.round(N * (0.15));
+		int top = N - stand;
+		int bottom = stand - 1;
+		int sum = 0;
+		int[] arr = new int[N];
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		Arrays.sort(arr);
+		for (int i = 0; i < N; i++) {
+			if (i >= stand && i < top) {
+				sum += arr[i];
+			}
+		}
+		System.out.println(Math.round((double) sum / (N - (stand * 2))));
+	}
+}
