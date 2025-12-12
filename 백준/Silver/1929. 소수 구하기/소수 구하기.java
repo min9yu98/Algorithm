2069a697acc5c6bae7a3dfca7b6ed10605e40753
MinGyu <kimmingyu9811@gmail.com>
2025-12-12 13:08:36 +0900

    [Silver III] Title: 소수 구하기, Time: 964 ms, Memory: 28800 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -0,0 +1,36 @@
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	private static boolean[][] arr;
+	private static int min = Integer.MAX_VALUE;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int M = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+
+		for (int i = M; i <= N; i++) {
+			if (isPrime(i)) {
+				System.out.println(i);
+			}
+		}
+	}
+
+	private static boolean isPrime(int n) {
+		if (n < 2) return false;
+		for (int i = 2; i <= Math.sqrt(n); i++) {
+			if (n % i == 0) return false;
+		}
+		return true;
+	}
+}
