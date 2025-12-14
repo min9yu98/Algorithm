61c050f17d518d0021b2bd6836e8a0564d23b099
MinGyu <kimmingyu9811@gmail.com>
2025-12-14 23:43:46 +0900

    [Silver IV] Title: ATM, Time: 116 ms, Memory: 14468 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.java
@@ -0,0 +1,35 @@
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.StringTokenizer;
+import java.util.stream.Collectors;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		int tmp = arr[0];
+		int result = arr[0];
+		for (int i = 1; i < N; i++) {
+			tmp += arr[i];
+			result += tmp;
+		}
+		System.out.println(result);
+	}
+}
