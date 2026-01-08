8796417edd2caba32e662f78bc0c9b680e4b6555
MinGyu <kimmingyu9811@gmail.com>
2026-01-08 21:36:38 +0900

    [Silver I] Title: 절댓값 힙, Time: 448 ms, Memory: 25892 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -0,0 +1,44 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				if (Math.abs(o1) > Math.abs(o2)) {
+					return Math.abs(o1) - Math.abs(o2);
+				} else if (Math.abs(o1) == Math.abs(o2)) {
+					return o1 - o2;
+				} else {
+					return -1;
+				}
+			}
+		});
+		for (int i = 0; i < N; i++) {
+			int x = Integer.parseInt(br.readLine());
+			if (x == 0) {
+				if (pq.isEmpty()) {
+					System.out.println(0);
+				} else {
+					System.out.println(pq.poll());
+				}
+			} else {
+				pq.offer(x);
+			}
+		}
+	}
+}
