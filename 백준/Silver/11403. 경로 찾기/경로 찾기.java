5f6882ea94f875d0e1f4c16ee4a04ae51e50a6f7
MinGyu <kimmingyu9811@gmail.com>
2026-01-15 11:47:54 +0900

    [Silver I] Title: 경로 찾기, Time: 156 ms, Memory: 16888 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.java
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -2,8 +2,11 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
+import java.util.Deque;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -36,11 +39,16 @@ public class Main {
 			}
 		}
 
+		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
-				System.out.print(arr[i][j] + " ");
+				sb.append(arr[i][j]).append(" ");
 			}
-			System.out.println();
+			sb.append("\n");
 		}
+
+		System.out.println(sb.toString());
 	}
+
+
 }
