3c6bae1a26ac35be2ea1e8a6d295d147726e6fde
MinGyu <kimmingyu9811@gmail.com>
2026-04-05 01:54:53 +0900

    [Silver III] Title: 계단 오르기, Time: 104 ms, Memory: 14100 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,28 +1,42 @@
-import java.io.*;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.io.IOException;
 
 public class Main {
 
-	static int[] arr = new int[301];
-	static int[] dp = new int[301];
+	static int N;
+	static Integer[] dp;
+	static int[] arr;
 
-	public static void main(String[] args) throws IOException{
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int n = Integer.parseInt(br.readLine());
-		for (int i = 0; i < n; i++) {
+		N = Integer.parseInt(br.readLine());
+		arr = new int[N + 1];
+		dp = new Integer[N + 1];
+
+		for (int i = 1; i < N + 1; i++) {
 			arr[i] = Integer.parseInt(br.readLine());
 		}
 
 		dp[0] = arr[0];
-		dp[1] = Math.max(arr[0] + arr[1], arr[1]);
-		dp[2] = Math.max(arr[0] + arr[2], arr[1] + arr[2]);
+		dp[1] = arr[1];
 
-		for (int i = 3; i < n; i++) {
-			dp[i] = Math.max(arr[i] + dp[i - 2], arr[i] + arr[i - 1] + dp[i - 3]);
+		if (N >= 2) {
+			dp[2] = arr[1] + arr[2];
 		}
 
-		System.out.println(dp[n - 1]);
-
+		System.out.println(find(N));
 	}
 
+	private static int find(int n) {
+		if (dp[n] == null) {
+			dp[n] = Math.max(find(n - 2), find(n - 3) + arr[n - 1]) + arr[n];
+		}
+
+		return dp[n];
+	}
 }
+
