a6f2f151f630db91902f1496fabe4dabf0aa6908
MinGyu <kimmingyu9811@gmail.com>
2026-04-14 20:53:37 +0900

    [Silver V] Title: 집합, Time: 1044 ms, Memory: 313188 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -1,43 +1,47 @@
-import java.io.*;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.*;
+import java.io.IOException;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		int n = Integer.parseInt(br.readLine());
-		StringTokenizer st;
-		Set<Integer> map = new HashSet<>();
-		int bit = 0;
-		for (int i = 0; i < n; i++) {
-			st = new StringTokenizer(br.readLine());
-			String input = st.nextToken();
-			if (input.equals("all")) {
-				bit = (1 << 21) - 1;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int M = Integer.parseInt(br.readLine());
+		int mask = 0;
+		for (int i = 0; i < M; i++) {
+			String str = br.readLine();
+
+			if ("all".equals(str)) {
+				mask = (1 << 21) - 2;
 				continue;
-			} else if (input.equals("empty")) {
-				bit = 0;
+			} else if ("empty".equals(str)) {
+				mask = 0;
 				continue;
 			}
+
+			StringTokenizer st = new StringTokenizer(str);
+			String comm = st.nextToken();
 			int num = Integer.parseInt(st.nextToken());
-			switch (input) {
-				case "add":
-					bit |= (1 << num);
-					break;
-				case "remove":
-					bit &= ~(1 << num);
-					break;
-				case "check":
-					sb.append((bit & (1 << num)) != 0 ? 1 : 0).append("\n");
-					break;
-				case "toggle":
-					bit ^= (1 << num);
-					break;
-				default:
-					break;
+			if ("add".equals(comm)) {
+				mask |= (1 << num);
+			} else if ("remove".equals(comm)) {
+				mask &= ~(1 << num);
+			} else if ("check".equals(comm)) {
+				boolean flag;
+				sb.append((mask & (1 << num)) != 0 ? 1 : 0).append('\n');
+			} else if ("toggle".equals(comm)) {
+				if ((mask & (1 << num)) != 0) {
+					mask &= ~(1 << num);
+				} else {
+					mask |= (1 << num);
+				}
 			}
 		}
 		System.out.println(sb);
 	}
+
 }
+
